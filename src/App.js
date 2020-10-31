2368c1b7db0323a4554478399ee832ef8b6533ed
alrafi <hafisalrafi@gmail.com>
2020-10-31 19:12:58 +0700

    add  movie detail route

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Home from './components/Home/Home';
 import './assets/styles/index.scss';
+import MovieDetail from './components/MovieDetail/MovieDetail';
 
 const App = () => {
   return (
     <div>
       <Router>
         <Switch>
+          <Route path='/movie/:id' component={MovieDetail} exact />
           <Route path="/" component={Home} exact />
         </Switch>
       </Router>
