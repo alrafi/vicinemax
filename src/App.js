708b0dd407dabe02d44b57281ec3e4fa1ec734b0
alrafi <hafisalrafi@gmail.com>
2020-10-31 13:08:37 +0700

    add route app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Home from './components/Home/Home';
+import './assets/styles/index.scss';
 
 const App = () => {
   return (
     <div>
-      <h1>Vicinemax</h1>
+      <Router>
+        <Switch>
+          <Route path="/" component={Home} exact />
+        </Switch>
+      </Router>
     </div>
   );
 };
