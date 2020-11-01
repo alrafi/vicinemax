f3220133145ebe6bde9c096fb803de354d2aec27
alrafi <hafisalrafi@gmail.com>
2020-11-01 19:46:12 +0700

    update loading spinner

--- src/components/MovieDetail/MovieDetail.js
+++ src/components/MovieDetail/MovieDetail.js
@@ -1,5 +1,7 @@
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import tmdb from '../../api/tmdb';
+import { css } from "@emotion/core";
+import BounceLoader from "react-spinners/BounceLoader";
 
 const MovieDetail = () => {
   const [detail, setDetail] = useState(null)
@@ -16,11 +18,14 @@ const MovieDetail = () => {
 
   const baseUrl = 'http://image.tmdb.org/t/p/w185';
 
-  if (!detail) return <p>Loading...</p>;
+  if (!detail) return (<BounceLoader
+    size={100}
+    color={"#08919a"}
+  />);
 
   return (
     <div>
-      <img src={`${baseUrl}${detail.poster_path}`} alt=""/>
+      <img src={`${baseUrl}${detail.poster_path}`} alt="" />
       <h1>{detail.title}</h1>
       <p>{detail.overview}</p>
     </div>
