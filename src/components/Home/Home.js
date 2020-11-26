394b2f6f6bfd5367226901024af370f005239bc9
alrafi <hafisalrafi@gmail.com>
2020-11-26 12:35:43 +0700

    remove console log

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -39,7 +39,6 @@ const Home = () => {
             language: 'en-US'
           }
         })
-        // console.log(res.data.genres)
         setGenres(res.data.genres)
       } catch (err) {
 
@@ -54,7 +53,6 @@ const Home = () => {
   const getRandom = (allMovies) => {
     const movies = [...allMovies]
     let random = movies.sort(() => .5 - Math.random()).slice(0, 4)
-    console.log(random)
     setFeaturedMovies(random);
   }
 
@@ -63,7 +61,6 @@ const Home = () => {
   }
 
   const getMovieGenre = (genreIDs) => {
-    // console.log(genreIDs)
     let arr = []
     if (genres.length <= 0) return arr;
 
@@ -71,7 +68,6 @@ const Home = () => {
       const genreItem = genres.filter(genre => genre.id === item)
       arr.push(genreItem[0])
     })
-    // console.log(arr)
     return arr;
   }
 
@@ -92,21 +88,21 @@ const Home = () => {
             featuredMovies.map(movie => {
               return (
                 <React.Fragment key={movie.id}>
-                  <span/>
+                  <span />
                   <div className="featured-wrapper" style={background(movie.backdrop_path)}></div>
                   <div className="desc">
                     <Link to={`/movie/${movie.id}`} className="link-title">
-                        <h1 className="title">{movie.title}</h1>
+                      <h1 className="title">{movie.title}</h1>
                     </Link>
                     <div className="info">
-                        {/* <Rating rate={this.props.movie.rate}/> */}
-                        <h5>{`${movie.vote_average} rating`}</h5>
-                        <h5>{`${movie.vote_count} reviews`}</h5>
-                        {/* <h5 className="duration">{movie.duration}</h5> */}
-                        <h5 className="year">{getReleasedYear(movie.release_date)}</h5>
+                      {/* <Rating rate={this.props.movie.rate}/> */}
+                      <h5>{`${movie.vote_average} rating`}</h5>
+                      <h5>{`${movie.vote_count} reviews`}</h5>
+                      {/* <h5 className="duration">{movie.duration}</h5> */}
+                      <h5 className="year">{getReleasedYear(movie.release_date)}</h5>
                     </div>
                     <p>
-                        {movie.overview}
+                      {movie.overview}
                     </p>
                     {/* {this.renderTrailerButton(this.props.onTrailerPress)} */}
                   </div>
@@ -118,35 +114,35 @@ const Home = () => {
 
       </div>
       <div className="popular-container">
-          <h2>Popular</h2>
-          <div className="movies-wrapper">
-            {movies.map((movie) => {
-              return (
-                <div className="movie-item-container" key={movie.id}>
-                  <Link to={`/movie/${movie.id}`} >
-                    <img
-                      className="movie-poster"
-                      src={`${baseUrl}${movie.poster_path}`}
-                      alt={movie.title}
-                      loading="lazy"
-                    />
-                    <p className="movie-title">{movie.title} ({getReleasedYear(movie.release_date)})</p>
-                  </Link>
-                  <div className="genre-wrapper">
-                    {
-                      getMovieGenre(movie.genre_ids).map(item => {
-                        return (<p className="movie-genres" key={`${movie.id}-${item.id}`}>
-                          {item.name}<span>, </span>
-                        </p>
-                        )
-                      })
-                    }
-                  </div>
+        <h2>Popular</h2>
+        <div className="movies-wrapper">
+          {movies.map((movie) => {
+            return (
+              <div className="movie-item-container" key={movie.id}>
+                <Link to={`/movie/${movie.id}`} >
+                  <img
+                    className="movie-poster"
+                    src={`${baseUrl}${movie.poster_path}`}
+                    alt={movie.title}
+                    loading="lazy"
+                  />
+                  <p className="movie-title">{movie.title} ({getReleasedYear(movie.release_date)})</p>
+                </Link>
+                <div className="genre-wrapper">
+                  {
+                    getMovieGenre(movie.genre_ids).map(item => {
+                      return (<p className="movie-genres" key={`${movie.id}-${item.id}`}>
+                        {item.name}<span>, </span>
+                      </p>
+                      )
+                    })
+                  }
                 </div>
-              );
-            })}
-          </div>
+              </div>
+            );
+          })}
         </div>
+      </div>
     </Layout>
   );
 };
--- src/components/SearchResult/SearchResult.js
+++ src/components/SearchResult/SearchResult.js
@@ -1,12 +1,11 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import Layout from '../Layout/Layout';
 import tmdb from '../../api/tmdb'
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import bgAlt from '../../assets/img/bg_alt.png'
 
 const SearchResult = (props) => {
-  const {match} = props
-  console.log(props)
+  const { match } = props
   const [dataSearch, setDataSearch] = useState([])
   const [genres, setGenres] = useState([])
 
@@ -19,7 +18,6 @@ const SearchResult = (props) => {
           },
         });
         setDataSearch(res.data.results);
-        console.log(res.data.results)
       } catch (err) {
         console.log(err);
         return;
@@ -43,7 +41,6 @@ const SearchResult = (props) => {
             language: 'en-US'
           }
         })
-        // console.log(res.data.genres)
         setGenres(res.data.genres)
       } catch (err) {
 
@@ -53,7 +50,6 @@ const SearchResult = (props) => {
   }, [])
 
   const getMovieGenre = (genreIDs) => {
-    // console.log(genreIDs)
     let arr = []
     if (genres.length <= 0) return arr;
 
@@ -61,7 +57,6 @@ const SearchResult = (props) => {
       const genreItem = genres.filter(genre => genre.id === item)
       arr.push(genreItem[0])
     })
-    // console.log(arr)
     return arr;
   }
 
@@ -70,35 +65,35 @@ const SearchResult = (props) => {
       <p>Search Results</p>
       <h1>{match.params.query}</h1>
       <div className="popular-container">
-          <h2>Popular</h2>
-          <div className="movies-wrapper">
-            {dataSearch.map((movie) => {
-              return (
-                <div className="movie-item-container" key={movie.id}>
-                  <Link to={`/movie/${movie.id}`} >
-                    <img
-                      className="movie-poster"
-                      src={movie.poster_path ? `${baseUrl}${movie.poster_path}` : bgAlt}
-                      alt={movie.title}
-                      loading="lazy"
-                    />
-                    <p className="movie-title">{movie.title} {movie.release_date ? `(${(getReleasedYear(movie.release_date))})` : ""}</p>
-                  </Link>
-                  <div className="genre-wrapper">
-                    {
-                      getMovieGenre(movie.genre_ids).map(item => {
-                        return (<p className="movie-genres" key={`${movie.id}-${item.id}`}>
-                          {item.name}<span>, </span>
-                        </p>
-                        )
-                      })
-                    }
-                  </div>
+        <h2>Popular</h2>
+        <div className="movies-wrapper">
+          {dataSearch.map((movie) => {
+            return (
+              <div className="movie-item-container" key={movie.id}>
+                <Link to={`/movie/${movie.id}`} >
+                  <img
+                    className="movie-poster"
+                    src={movie.poster_path ? `${baseUrl}${movie.poster_path}` : bgAlt}
+                    alt={movie.title}
+                    loading="lazy"
+                  />
+                  <p className="movie-title">{movie.title} {movie.release_date ? `(${(getReleasedYear(movie.release_date))})` : ""}</p>
+                </Link>
+                <div className="genre-wrapper">
+                  {
+                    getMovieGenre(movie.genre_ids).map(item => {
+                      return (<p className="movie-genres" key={`${movie.id}-${item.id}`}>
+                        {item.name}<span>, </span>
+                      </p>
+                      )
+                    })
+                  }
                 </div>
-              );
-            })}
-          </div>
+              </div>
+            );
+          })}
         </div>
+      </div>
     </Layout>
   );
 };
